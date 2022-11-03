2dff81a311acaf596052c2bca84a716d537890d5
Jaden L. Phillips <jlp1214@student.ubc.ca>
2022-11-03 09:51:44 -0700

    Added wquare function with documentation

---
+++ tests/testthat/test-square.R
@@ -0,0 +1,24 @@
+library(devtools)
+library(testthat)
+library(square)
+
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
+
+
+test_that("Basic functionality test", {
+  expect_equal(2 * 2, 4)
+})
+load_all()
+square(3)
+expect_equal(square(3), 9)
+square(1:3)
+
+test_that("Basic functionality test", {
+  expect_equal(square(3), 9)
+  expect_equal(square(1:3),
+               c(1,4,9))
+})
+
+test()
